ccc070f9f28564eda09fbc77081f448d107bd17e
justincode98 <test>
2022-08-05 18:05:12 -0700

    simple review template

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,1 +1,33 @@
 const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const user = require("./User");
+
+
+const ReviewSchema = new Schema(
+    {
+        rating: {
+            type: Number,
+            required: true            
+        },
+        reviewText: { 
+            type: String,
+            required: true
+        },
+       user_id: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: "User"
+            }
+          ]
+
+    },
+    {
+        toJSON: {
+            getters: true//reminder virutals are like pseudofields
+        }
+    }
+);
+
+const Review = model("Review", ReviewSchema);
+
+module.exports = Review;
